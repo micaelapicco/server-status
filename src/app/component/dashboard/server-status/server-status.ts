6051bb12189c88f694ae1cc0b749a284f7677f1d
Micaela Picco <mpicco@ica.com.uy>
2025-12-11 16:51:04 -0300

    ToDo: arreglar el la clase dinamica del server status

--- src/app/component/dashboard/server-status/server-status.ts
+++ src/app/component/dashboard/server-status/server-status.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { DashboardItem } from "../dashboard-item/dashboard-item";
 
 @Component({
   selector: 'app-server-status',
-  imports: [DashboardItem],
+  imports: [DashboardItem, CommonModule],
   standalone: true,
   templateUrl: './server-status.html',
   styleUrl: './server-status.css',
 })
 export class ServerStatus {
-  currentStatus = 'online';
+currentStatus = 'online'
+constructor() {  
+  
+  setInterval(() => {
+    const rand = Math.random();
+    if (rand < 0.4) {
+      this.currentStatus = 'online';
+    } else if (rand < 0.8) {
+      this.currentStatus = 'offline';
+    } else {
+      this.currentStatus = 'unknown';
+    }
+       console.log('Status actual:', this.currentStatus);
+    }, 5000);
+  } 
 }
+
+
